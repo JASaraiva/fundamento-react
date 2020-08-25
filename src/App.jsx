0b14d24b4332845f47ca8ea1d8f7211264e4e860
Jonas Almeida <jo.almeida@reply.com>
2020-08-25 08:29:22 -0300

    segunda aula

--- src/App.jsx
+++ src/App.jsx
@@ -9,15 +9,30 @@ import Repeticao from './components/basicos/Repeticao.jsx'
 import Condicional from "./components/basicos/Condicional"
 import Pai from "./components/comunicacao/direta/Pai.jsx"
 import Super from "./components/comunicacao/indireta/Super"
+import Input from "./components/form/Input"
 
 export default props =>
     <div className="App">
-        <Card titulo="Primeiro Componente">
-            <Primeiro></Primeiro>
+        <Card titulo="Setimo Componente">
+           <Input></Input>
         </Card>
-        <Card titulo="Segundo Componente">
-            <Segundo titulo='meu titulo' subtitulo='meu subtitulo'></Segundo>
+
+         <Card titulo="Setimo Componente">
+           <Super></Super>
         </Card>
+
+        <Card titulo="Sexto Componente">
+           <Pai></Pai>
+        </Card>
+
+        <Card titulo="Quinto Componente">
+           <Condicional numero={1}></Condicional>
+        </Card>
+
+        <Card titulo="Quarto Componente">
+           <Repeticao></Repeticao>
+        </Card>
+        
         <Card titulo="Terceiro Componente">
             <Terceiro>
                 <ul>
@@ -27,22 +42,11 @@ export default props =>
             </Terceiro>
         </Card>
 
-        <Card titulo="Quarto Componente">
-           <Repeticao></Repeticao>
+        <Card titulo="Segundo Componente">
+            <Segundo titulo='meu titulo' subtitulo='meu subtitulo'></Segundo>
         </Card>
 
-
-        <Card titulo="Quinto Componente">
-           <Condicional numero={1}></Condicional>
-        </Card>
-
-
-        <Card titulo="Sexto Componente">
-           <Pai></Pai>
-        </Card>
-
-
-        <Card titulo="Setiimo Componente">
-           <Super></Super>
+        <Card titulo="Primeiro Componente">
+            <Primeiro></Primeiro>
         </Card>
     </div>
